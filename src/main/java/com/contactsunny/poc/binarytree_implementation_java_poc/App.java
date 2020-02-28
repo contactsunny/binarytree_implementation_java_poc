dbb8d076953760250b024a2177484ce8f8faa8ae
Sunny Srinidhi <sunny.3mysore@gmail.com>
2020-02-28 16:24:28 +0530

    Added comment.

--- src/main/java/com/contactsunny/poc/binarytree_implementation_java_poc/App.java
+++ src/main/java/com/contactsunny/poc/binarytree_implementation_java_poc/App.java
@@ -3,5 +3,6 @@ package com.contactsunny.poc.binarytree_implementation_java_poc;
 public class App {
 
     public static void main(String[] args) {
+        // Nothing to do here, because we now have unit tests.
     }
 }
